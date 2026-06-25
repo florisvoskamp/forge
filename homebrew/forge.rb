071c6091141f60aba2d57ce0c5004c310024d2bc
florisvoskamp <floris.voskamp@outlook.com>
2026-06-25 14:54:20 +0200

    chore: prepare v0.4.4 release

--- homebrew/forge.rb
+++ homebrew/forge.rb
@@ -9,24 +9,24 @@
 class Forge < Formula
   desc "Multi-provider mesh AI coding CLI"
   homepage "https://github.com/florisvoskamp/forge"
-  version "0.4.3" # release: update sha256 values from checksums.txt after the tag workflow finishes
+  version "0.4.4" # release: update sha256 values from checksums.txt after the tag workflow finishes
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-aarch64-apple-darwin.tar.gz"
-      sha256 "03b04807f61241f42d30386da5c4866a83859c209fa2146dbd5408008cd439eb"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-apple-darwin.tar.gz"
-      sha256 "825ea813cfec84b4dbc1252a5465eaa3360775c0a9f34b4361d1ec74627f4f66"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e2792668ca4ed313361e1d88c0713126ad87471be8e9d5c02b03ddd18ad41c33"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
